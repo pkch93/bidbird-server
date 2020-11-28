c72e2e7f80e44f70d40faf0db57747914dec05c5
pkch93 <pkch9377@gmail.com>
2020-11-28 15:41:47 +0900

    refactor: frontend 프로젝트 server 모듈에 추가

---
+++ server/src/front/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from '@/App'
+
+import '@/shared/reset.css'
+import '@/shared/shared.css'
+
+ReactDOM.render(<App />, document.getElementById('root'))
