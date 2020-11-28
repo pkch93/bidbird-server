5d9c54f5118f55379ca7364e2e4ea32e47b7ceba
pkch93 <pkch9377@gmail.com>
2020-11-28 15:40:38 +0900

    refactor: api 모듈 server로 변경

---
+++ server/src/main/java/com/propgrammers/bidbird/api/BidBirdApplication.java
@@ -0,0 +1,12 @@
+package com.propgrammers.bidbird.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidBirdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidBirdApplication.class, args);
+    }
+}
